27e96d414d8bab7763735bb50df15fe59db5b752
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2025-04-18 20:50:18 +0530

    feat(fibinocci.js): Added iterative Fibonacci implementation.

---
+++ fibinocci.js
@@ -0,0 +1,10 @@
+function fibonacci(n) {
+    if (n === 0 || n === 1) return n;
+    let a = 0, b = 1;
+    for (let i = 2; i <= n; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
