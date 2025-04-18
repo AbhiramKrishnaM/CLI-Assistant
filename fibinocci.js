f119ed037a8ab05e4d5d08f47bfeee934b06be7a
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2025-04-18 20:35:16 +0530

    feat(fibonacci.js): Added efficient Fibonacci calculation implementation using iteration.

---
+++ fibinocci.js
@@ -0,0 +1,10 @@
+function fibonacci(n) {
+    if (n === 0) return 0;
+    let a = 0, b = 1;
+    for (let i = 2; i <= n; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
